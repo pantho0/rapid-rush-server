ae8f5e68a304fcd2acae64731aa2c143f1410a82
pantho0 <panthomashreky6@gmail.com>
2023-11-28 16:26:10 +0600

    slightly updated

--- index.js
+++ index.js
@@ -25,11 +25,38 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
+
+    const parcelsCollection = client.db('RapidRush').collection('percels')
+
+    app.post("/parcels", async(req,res)=>{
+        try{
+            const parcel = req.body;
+            const result = await parcelsCollection.insertOne(parcel)
+            res.send(result);
+        }catch(error){
+            console.log(error);
+        }
+    })
+
+
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+
+
+    
+
+
+
+
+
+
+
+
+
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
